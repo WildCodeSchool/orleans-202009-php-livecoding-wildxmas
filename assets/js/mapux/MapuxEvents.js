f61de2e1260aa139498c84f153b5a6fe82763a3d
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-01-12 14:48:42 +0100

    upload with vich

--- assets/js/mapux/MapuxEvents.js
+++
@@ -1,9 +0,0 @@
-require ('../../../node_modules/leaflet/dist/leaflet')
-
-export class MapuxEvents {
-    constructor(target, map, icons) {
-        this.target = target // Should be the map or a marker
-        this.map = map
-        this.icons = icons
-    }
-}
